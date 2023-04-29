a6184a6658fa9cd26e6b9d98c00ff651f6ba986b
Jesus <jesucal68@gmail.com>
2023-04-29 16:42:39 -0600

    Agregue useTodos y README

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,54 +1,24 @@
-import { useEffect, useReducer } from "react";
-import { todoReducer } from "./todoReducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
-
-const initialState = [
-  // {
-  //   id: new Date().getTime(),
-  //   description: "Tarea 1",
-  //   done: false,
-  // }
-];
-
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos') || [])
-}
+import { useTodos } from "../hooks";
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-
-  }, [todos])
-  
-
-
-
-  const handleNewTodo = (todo) => {
-    const action = {
-      type: '[TODO] Add Todo',
-      payload: todo,
-    }
-    dispatch(action);
-  }
-
-  const handleDeleteTodo = ( id ) => {
-    dispatch({
-      type: '[TODO] Remove Todo',
-      payload: id
-    });
-  }
+  const { handleDeleteTodo, handleNewTodo, onToggleTodo, todos, todosCount, pendingTodosCount } = useTodos();
 
   return (
     <>
-      <h1>TodoApp 10, <small>pendientes: 2 </small></h1>
+      <h1>
+        TodoApp {todosCount}, <small>pendientes: {pendingTodosCount} </small>
+      </h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} onDeleteTodo={ handleDeleteTodo}/>
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={onToggleTodo}
+          />
         </div>
         <div className="col-5">
           <h4>Agregar TODO</h4>
--- src/08-useReducer/TodoItem.jsx
+++ src/08-useReducer/TodoItem.jsx
@@ -1,8 +1,8 @@
-export const TodoItem = ({ todo, onDeleteTodo }) => {
+export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
   return (
     <>
       <li className="list-group-item d-flex justify-content-between">
-        <span className="align-self-center">{todo.description}</span>
+        <span onDoubleClick={ () => {onToggleTodo(todo.id)} } className={`align-self-center ${(todo.done) ? 'text-decoration-line-through' : ''}`}>{todo.description}</span>
         <button className="btn btn-danger" onClick={ () => onDeleteTodo(todo.id) }>Borrar</button>
       </li>
     </>
